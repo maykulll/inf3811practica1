9cab1ae8a9323d5a8336876a71618a9cf77d31e7
maykulll <45230781+maykulll@users.noreply.github.com>
2021-04-08 12:24:25 -0400

    Creando mas rutas en el proyecto

--- port/run.py
+++ port/run.py
@@ -3,5 +3,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return ("Hola Mundo")
+@app.route("/saludo")
+def saludo():
+    return ("Hola buenas tardes")
+@app.route("/usuario/<string:usuario>")
+def usuario():
+    return ("Hola: "+usuario)
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
