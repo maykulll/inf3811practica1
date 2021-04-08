1ceaaf033eefb1bc2985d16ee6ba068488095b53
maykulll <45230781+maykulll@users.noreply.github.com>
2021-04-08 12:52:21 -0400

    Actualizando las rutas de la pagina

--- port/run.py
+++ port/run.py
@@ -2,13 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return ("Hola Mundo")
+    return ("BIENVENIDO A LA PRUEBA PARA CREAR FLASK CON PYTHON")
 @app.route("/saludo")
 def saludo():
-    return ("Hola buenas tardes")
-@app.route("/usuario/<string:usuario>")
+    return ("Hola buenas tardes esta es una ruta donde se pueden crear otras vistas")
+@app.route("/usuario")
 def usuario():
-    return ("Hola: "+usuario)
+    return ("Hola usuario")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
